35f483b26fb64a791d6466f4726fb98317712f8e
Eduarduar <eduardoarcega1997@gmail.com>
2022-10-19 20:30:25 -0500

    ahora el create se pueden agregar imagenes y algunas correcciones mas

--- js/create.js
+++ js/create.js
@@ -2,8 +2,8 @@
 const btnClose = document.getElementById('close');
 const btnAdd = document.getElementById('add');
 const btnSave = document.getElementById('save');
-let NoItems = 0;
-let NoContainers = 0;
+let NoItems = 0; // contador de item agregados
+let NoContainers = 0; // contador de contenedores agregados
 
 //Funciones
 
@@ -13,13 +13,23 @@ const close = function () { // función que regresa a la pagina index
 
 const addCardItem = function () { // función queagrega un nuevo item o contenedor con item segun sea necesario
 
+    let inputHIde = document.createElement('input'); // creamos el input file que guardara la imagen para enviarla
+    inputHIde.setAttribute('type','file');
+    inputHIde.setAttribute('id',`imagen${NoItems + 1}`);
+    inputHIde.setAttribute('name',`imagen${NoItems + 1}`);
+    inputHIde.setAttribute('onchange','vista_preliminar(event)');
+
+    let divHide = document.querySelector('.hide'); // la agregamos al contenedor oculto dentro del form
+    divHide.appendChild(inputHIde);
+
     let div = document.createElement('div'); // login-card-item
     div.setAttribute('class','altar-card-item');
 
     let button = document.createElement('button'); // btnFoto
-    let text = document.createTextNode('Add photo');
+    button.innerHTML = `<label for="imagen${NoItems + 1}">Add photo</label>`;
     button.setAttribute('class','btnFoto');
-    button.appendChild(text);
+    button.setAttribute('id',`img-foto-${inputHIde.id}`);
+    button.setAttribute('type','button');
 
     let divContainerText = document.createElement('div'); // altar-card-item-text-container
     divContainerText.setAttribute('class','altar-card-item-text-container');
@@ -27,7 +37,7 @@ const addCardItem = function () { // función queagrega un nuevo item o contened
     let textarea = document.createElement('textarea'); // textarea
     textarea.setAttribute('class','text');
 
-    divContainerText.appendChild(textarea);
+    divContainerText.appendChild(textarea); // juntamos todo en orden correcto
     div.appendChild(button);
     div.appendChild(divContainerText);
 
@@ -36,18 +46,18 @@ const addCardItem = function () { // función queagrega un nuevo item o contened
         let divContainer = document.createElement('div'); // login-card-item-container
         divContainer.setAttribute('class','altar-card-item-container');
         divContainer.appendChild(div);
-        header.appendChild(divContainer);
+        header.appendChild(divContainer); // agregamos el item a un nuevo contenedor
         NoItems++;
         NoContainers++;
     }else{
-        let Containers = document.getElementsByClassName('altar-card-item-container');
+        let Containers = document.getElementsByClassName('altar-card-item-container'); // agregamos el item a contenedor correspondiente
         Containers[NoContainers].classList.add('flex');
         Containers[NoContainers].appendChild(div);
         NoItems++;
     }
 }
 
-const save = function () {
+const save = function () { // función que elimina los botones para que la pagina quede libre de botones y tambien se encargara de guardar
     let add = document.querySelector('.altar-card-item-button-add-container span');
     add.parentNode.removeChild(add);
 
@@ -58,6 +68,25 @@ const save = function () {
     textarea.forEach((e) => {
         e.classList.add('active');
     });
+}
+
+let vista_preliminar = (event) => { // función que se encarga de mostrar la imagen cargada en su contenedor corresponienten segun el id de su input file
+    let leer_img = new FileReader();
+    let id_img = document.getElementById(`img-foto-${event.target.id}`);
+
+    leer_img.onload = () => {
+
+       if (leer_img.readyState == 2){
+           
+           id_img.style = `background-color: #fff0;border: none;`;
+           id_img.innerHTML = `<img src='${leer_img.result}'  class="img">`;
+
+       }
+
+   }
+
+   leer_img.readAsDataURL(event.target.files[0])
+
 }
 
 // Listeners
